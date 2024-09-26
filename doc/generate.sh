8c659dc4a0f0a74aa5b017d58808c8d25b46cd2a
Jacki <jacki@thejackimonster.de>
2024-09-26 04:35:31 +0200

    Update doxygen version automatically
    Signed-off-by: Jacki <jacki@thejackimonster.de>

--- doc/generate.sh
+++ doc/generate.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 cd "${MESON_SOURCE_ROOT}"
+sed -i 's/PROJECT_NUMBER\(\s\+=\s\+\)\([0-9\.a-z\-]\+\)/PROJECT_NUMBER\1'"$(sh contrib/get_version.sh)/" 'Doxyfile'
 doxygen 'Doxyfile'
 cd doc/latex
 make
